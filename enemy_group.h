bb2bb9f0505e9d5636403618c093f5aabdf0d1d2
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-05-09 18:39:48 +0200

    Upload New File

---
+++ enemy_group.h
@@ -0,0 +1,14 @@
+#ifndef ENEMY_GROUP_H
+#define ENEMY_GROUP_H
+#include "npc.h"
+
+class enemy_group
+{
+public:
+    enemy_group(), enemy_group(npc *npcs);
+    npc *get_npc_adress(short int r, short int p);
+private:
+    npc formation_[2][2];
+};
+
+#endif // ENEMY_GROUP_H
