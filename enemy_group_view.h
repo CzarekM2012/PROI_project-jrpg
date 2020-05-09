5263cabb32408e5175c85d012327c54a1c7f6c76
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-05-09 18:40:04 +0200

    Upload New File

---
+++ enemy_group_view.h
@@ -0,0 +1,12 @@
+#ifndef ENEMY_GROUP_VIEW_H
+#define ENEMY_GROUP_VIEW_H
+#include "group_view.h"
+
+class enemy_group_view: public group_view
+{
+public:
+    enemy_group_view(), enemy_group_view(enemy_group *enemy_group);
+private:
+};
+
+#endif // ENEMY_GROUP_VIEW_H
