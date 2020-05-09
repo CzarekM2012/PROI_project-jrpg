23b5d937b72a9ee29c080577b2f2b4520f4a69cc
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-05-09 18:44:21 +0200

    Upload New File

---
+++ party_view.h
@@ -0,0 +1,12 @@
+#ifndef PARTY_VIEW_H
+#define PARTY_VIEW_H
+#include "group_view.h"
+
+class party_view: public group_view
+{
+public:
+    party_view(), party_view(party *party);
+private:
+};
+
+#endif // PARTY_VIEW_H
