afc89fb0f6e0f1b47d6074a56de8fedeb05fdeba
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-05-14 01:21:31 +0200

    Upload New File

---
+++ data_processing.cpp
@@ -0,0 +1,28 @@
+void int_to_char_array(int value, char* destination)
+{
+    int i=0;
+    if(value == 0)
+    {
+        destination[i++]='0';
+        destination[i]=0;
+        return;
+    }
+
+    while(value != 0)
+    {
+        int temp = value % 10;
+        destination[i++] = temp + '0';
+        value /= 10;
+    }
+    destination[i] = 0;
+    int start = 0;
+    i--;
+    while(start < i)
+    {
+        char temp = *(destination+start);
+        *(destination + start) = *(destination + i);
+        *(destination + i) = temp;
+        start++;
+        i--;
+    }
+}
