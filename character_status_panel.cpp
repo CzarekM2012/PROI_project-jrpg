fc4ac1d8c4f2b36c4634ab4047887c1729b584ab
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-05-14 01:24:08 +0200

    Update character_status_panel.cpp

--- character_status_panel.cpp
+++ character_status_panel.cpp
@@ -1,4 +1,5 @@
 #include "character_status_panel.h"
+#include "data_processing.cpp"
 
 character_status_panel::character_status_panel()
 {
@@ -23,42 +24,16 @@ void character_status_panel::paste_data(character *character)
 {
     // int/int to char array custom function(start from max_stat to speed up the process;
     name_->setText(*character->get_name());
-    set_current_stat_over_max_stat_text(hp_, character->get_current_hp(), character->get_max_hp());
-    set_current_stat_over_max_stat_text(sp_, character->get_current_sp(), character->get_max_sp());
-}
-
-void character_status_panel::set_current_stat_over_max_stat_text(QLabel *label, int nominator, int denominator)
-{
-    int mul = 10, number_of_digits = 1;
-    while(denominator/mul >= 1)
-    {
-        mul*=10;
-        number_of_digits++;
-    }
-    mul/=10;
-    char array[2*number_of_digits+2];
-    array[number_of_digits] = '/';
-    array[2*number_of_digits+1] = 0;
-    int i=0;
-    bool nominator_left_side_zero = true;
-    while(mul > 0)
-    {
-        char nominator_digit = nominator/mul, denominator_digit = denominator/mul;
-        nominator %= mul;
-        denominator %= mul;
-        mul/=10;
-        if(nominator_left_side_zero && nominator_digit == 0){nominator_digit = ' ';}
-        else
-        {
-            nominator_left_side_zero = false;
-            nominator_digit += '0';
-        }
-        denominator_digit += '0';
-        array[i]=nominator_digit;
-        array[number_of_digits+1+i]=denominator_digit;
-        i++;
-    }
-    label->setText(QString(array));
+    char *text1 = new char[12], *text2 = new char[12];
+    int temp = character->get_current_hp();
+    int_to_char_array(temp, text1);
+    int_to_char_array(character->get_max_hp(), text2);
+    hp_->setText(QString(text1) + QString("/") + QString(text2));
+    int_to_char_array(character->get_current_sp(), text1);
+    int_to_char_array(character->get_max_sp(), text2);
+    sp_->setText(QString(text1) + QString("/") + QString(text2));
+    delete [] text1;
+    delete [] text2;
 }
 
 QProgressBar *character_status_panel::get_action_bar_(){return action_;}
