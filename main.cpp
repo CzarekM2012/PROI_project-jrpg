a1cd463886a5b0a3109f3c7f9dab86980b0d2bad
Aleksandra Anna Piekarzewicz <a.piekarzewicz@stud.elka.pw.edu.pl>
2020-05-15 11:33:56 +0200

    Upload New File

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+#include "dialog_window.h"
+
+int main(int argc, char **argv)
+{
+ QApplication app (argc, argv);
+
+ dialog_window d_window;
+ d_window.show();
+
+ return app.exec();
+}
