5e0c16e6c83bbe9883c3d3f423a53a9ab7173027
Aleksandra Anna Piekarzewicz <a.piekarzewicz@stud.elka.pw.edu.pl>
2020-05-16 13:51:55 +0200

    Replace bglabel.h

--- bglabel.h
+++ bglabel.h
@@ -9,12 +9,29 @@ class BgLabel : public QWidget
 {
   Q_OBJECT
 public:
-  explicit BgLabel(QWidget *parent = nullptr);
+  explicit BgLabel(int part, QWidget *parent = nullptr);
+  int getSteps();
+  int getStepsCounter();
+
+private slots:
+ void slotButtonClicked();
 
 private:
+  int steps_counter;
+  int steps;
+  QString bg_pic;
+  std::vector<QString> face_set;
+  std::vector<QString> texts_set;
+  QLabel *background;
+  QLabel *dialog;
+  QLabel *text_bg;
   QLabel *face;
-  QLabel *text;
   QPushButton *next;
+
+  void setSteps(int);
+  static QString choose_bg(int);
+  static std::vector<QString> choose_face_set(int);
+  static std::vector<QString> choose_texts(int);
 
 };
 
