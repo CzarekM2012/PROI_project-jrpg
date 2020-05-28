4fa5fc341b2eea713501df82fbb80b5a924b8102
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-05-28 16:53:55 +0200

    Upload New File

---
+++ party_display.h
@@ -0,0 +1,12 @@
+#ifndef PARTY_DISPLAY_H
+#define PARTY_DISPLAY_H
+#include "switchable_frame.h"
+#include "character_frame.h"
+
+class party_display : public switchable_frame
+{
+public:
+    party_display();
+};
+
+#endif // PARTY_DISPLAY_H
