7a5e9e74c724628c35652d2e89b114a9137a9a3f
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-05-28 16:55:51 +0200

    Replace skill.h

--- skill.h
+++ skill.h
@@ -1,13 +1,28 @@
 #ifndef SKILL_H
 #define SKILL_H
-
+#include <QString>
 
 class skill
 {
 public:
-    skill();
+    skill(const char *name="normal attack", const char *description="", short type=1, short base=4, short affected=1, int power=0, int cost=0);
+    QString get_name();
+    QString get_description();
+    int get_base_power();
+    int get_sp_cost();
+    short get_base_stat();
+    short get_affected_stat();
+    bool is_attack();
+    bool aoe();
+    bool is_self_targettable();
+    bool revive();
+    bool operator==(skill skill);
 private:
-    int id_, base_power_, spcost_;
+    QString name_, description_;
+    int base_power_, spcost_;
+    short base_stat_, affected_stat_;
+    short type_;//single_target_attack, aoe_attack, single_target_self-targettable_support, single_target_non-self-targettable_support, aoe_support, single_target_revive
+                //         1                2                       5                                           6                           7                  8
 };
 
 #endif // SKILL_H
