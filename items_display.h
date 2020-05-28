6245669f9b34cc8f88a881b02811c3808e73f8bc
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-05-28 16:50:59 +0200

    Upload New File

---
+++ items_display.h
@@ -0,0 +1,13 @@
+#ifndef ITEMS_DISPLAY_H
+#define ITEMS_DISPLAY_H
+#include "switchable_frame.h"
+#include "item_frame.h"
+
+class items_display : public switchable_frame
+{
+public:
+    items_display();
+
+};
+
+#endif // ITEMS_DISPLAY_H
