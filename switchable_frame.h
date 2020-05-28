97641808a07ab1dc81d93123185c74c79658d012
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-05-28 16:56:40 +0200

    Replace switchable_frame.h

--- switchable_frame.h
+++ switchable_frame.h
@@ -8,16 +8,7 @@ class switchable_frame : public QFrame
 {
 public:
     switchable_frame();
-    void add_widget(QWidget *widget);
-    void add_widget(QWidget *widget, int row, int column, Qt::Alignment alignment = Qt::Alignment());
-    void add_widget(QWidget *widget, int row, int column, int rowspan, int columnspan, Qt::Alignment alignment = Qt::Alignment());
-    void set_stylesheet(const QString &stylesheet);
-    void get_margins(int *left, int *top, int *right, int *bottom);
-    void set_margins(int left, int top, int right, int bottom);
-    void set_spacing(int spacing);
-    void set_vSpacing(int spacing);
-    void set_hSpacing(int spacing);
-private:
+protected:
     QGridLayout *layout_;
 };
 
