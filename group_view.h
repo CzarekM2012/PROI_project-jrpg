68ff235cf3910c7666d4946210d69f5cd2869022
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-05-28 16:49:51 +0200

    Replace group_view.h

--- group_view.h
+++ group_view.h
@@ -1,28 +1,26 @@
 #ifndef GROUP_VIEW_H
 #define GROUP_VIEW_H
-#include <QFrame>
-#include <QGridLayout>
+#include "switchable_frame.h"
 #include "party.h"
 #include "enemy_group.h"
 #include "group_status_panel.h"
 #include "character_frame.h"
 
-class group_view: public QFrame
+class group_view: public switchable_frame
 {
+    Q_OBJECT
 public:
-    group_view();
+    group_view(switchable_frame *);
     character_frame *get_character_frame(short int r, short int p);
-    void place_frame(character_frame *frame, int row, int column, int rowspan, int columnspan);
-    void set_graphic(character_frame *frame, QString *new_graphic_file_name);
-    void set_graphics(party *party);
-    void set_graphics(enemy_group *enemy_group);
+    void set_graphics();
     void prepare_status_panels_highlight(group_status_panel *group_status_panel);
-    void prepare_action_target_acquisition();
-    void set_party(bool is_party);
+    void reset_states();
+private slots:
+    void pass_target_info(entity *);
+signals:
+    void target_info(entity *);
 private:
-    character_frame *formation_[2][2];
-    QGridLayout *layout_;
-    bool party_;
+    character_frame *formation_[2][2]={};
 };
 
 #endif // GROUP_VIEW_H
