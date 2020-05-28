2f733ec921ccadef8d6d0e78a9594ec157f2fee0
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-05-28 16:44:19 +0200

    Replace character_frame.cpp

--- character_frame.cpp
+++ character_frame.cpp
@@ -1,46 +1,82 @@
 #include "character_frame.h"
 
-character_frame::character_frame():QFrame()
+character_frame::character_frame():target_frame(), state_(0){}
+
+character_frame::character_frame(character *character, short initial_state):target_frame(), state_(initial_state){represented_object_ = character;}
+
+void character_frame::set_state(short value){state_ = value;}
+
+void character_frame::update(short target_state)
 {
-    targeting_ = false;
-    setMouseTracking(true);
-}
-
-character_frame::character_frame(character *represented_character):character_frame()
-{
-    represented_character_ = represented_character;
-}
-
-void character_frame::set_targeting(bool value){targeting_ = value;}
-
-void character_frame::set_represented_character(character *character){represented_character_ = character;}
-
-void character_frame::set_compound_style_sheet(QVector<QString> array_of_traits)
-{
-    QString stylesheet = array_of_traits[0];
-    for(int i=1; i<array_of_traits.size(); i++)
+    if(state_!=0)
     {
-        stylesheet+= QString("; ") + array_of_traits[i];
+        forced_update(target_state);
     }
-    setStyleSheet(stylesheet);
 }
 
-void character_frame::enterEvent(QEvent *event)
+void character_frame::forced_update(short target_state)
 {
-    emit hover_in();
-    QWidget::enterEvent(event);
+    set_state(target_state);
+    QWidget::update();
 }
-void character_frame::leaveEvent(QEvent *event)
-{
-    emit hover_out();
-    QWidget::leaveEvent(event);
-}
+
+short character_frame::state(){return state_;}
 
 void character_frame::mousePressEvent(QMouseEvent *event)
 {
-    if(targeting_)
-    {
-        emit target_acquired(represented_character_);
-    }
+    if(state_>5){emit target_acquired(represented_object_);}
     QWidget::mousePressEvent(event);
+}
+
+void character_frame::paintEvent(QPaintEvent *event)
+{
+    if(state_ > 1 && state_ !=6)
+    {
+        QPainter painter;
+        int w = width(), h = height();
+        painter.begin(this);
+        QBrush brush(Qt::BrushStyle::SolidPattern);
+        switch (state_)
+        {
+            case 2:{brush.setColor("lightblue"); break;}
+            case 3:{brush.setColor("darkblue"); break;}
+            case 4:{brush.setColor("red"); break;}
+            case 5:{brush.setColor("darkgreen"); break;}
+            case 7:{brush.setColor("lightblue"); break;}
+            case 8:{brush.setColor("yellow"); break;}
+            case 9:{brush.setColor("lightgreen"); break;}
+        }
+        painter.setPen(QPen(brush, 5));
+        painter.drawEllipse(QPoint(w/2, h/2), w/4, h/4);
+        if(state_ > 2 && state_ != 6)
+        {
+            painter.drawLine(w/2, h/4, w/2, h-h/4);
+            painter.drawLine(w/4, h/2, w-w/4, h/2);
+        }
+        painter.end();
+    }
+    QFrame::paintEvent(event);
+}
+
+void character_frame::is_target(entity *entity)
+{
+    if(state_ > 2)
+    {
+        if(entity == represented_object_)
+        {
+            if(state_ > 6)
+            {
+                state_ -= 4;
+                QWidget::update();
+            }
+        }
+        else
+        {
+            if(state_ < 6)
+            {
+                state_ += 4;
+                QWidget::update();
+            }
+        }
+    }
 }
