580fb4593727f52c24d818564a0ac84af21d0d47
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-05-28 16:53:33 +0200

    Replace party.h

--- party.h
+++ party.h
@@ -1,6 +1,7 @@
 #ifndef PARTY_H
 #define PARTY_H
 #include "pc.h"
+#include "item_storage.h"
 
 class party
 {
@@ -9,8 +10,12 @@ public:
     void add_member(pc *added_pc);
     pc *get_pc_address(int r, int p);
     void swap_members(pc *first_member, pc *second_member);
+    item_storage *get_item_storage();
 private:
+    void rewrite_from_pcs_and_delete_copies();
     pc formation_[3][2]={}; //frontline - backline - reserve
+    item_storage *item_storage_;
+    skill *available_skills[48]={};
 };
 
 #endif // PARTY_H
