2861b9c77f40583bde1359d6284487b00688dbb8
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-05-28 16:54:38 +0200

    Replace pc.h

--- pc.h
+++ pc.h
@@ -2,11 +2,15 @@
 #define PC_H
 #include "character.h"
 #include "npc.h"
+#include "equipment.h"
 class pc: public character
 {
 public:
-    pc(), pc(const char *name, const char *graphic_file_name, int *stats_array), pc(character *character), pc(pc *pc), pc(npc *npc);
+    pc(), pc(const char *name, const char *description, unsigned int *stats_array, equipment **gear_array);
+    pc(character *character), pc(pc *pc), pc(npc *npc);
+    equipment *get_part_of_ecquipment(int identifier);
 private:
+    equipment *equipment_[7]={};
 };
 
 #endif // PC_H
