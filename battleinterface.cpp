9fafa4394e6be9757f326ae774372961762d5eee
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-05-28 16:43:20 +0200

    Replace battleinterface.cpp

--- battleinterface.cpp
+++ battleinterface.cpp
@@ -1,15 +1,13 @@
 #include "battleinterface.h"
 
-battleinterface::battleinterface(party *travel_party, enemy_group enemy_group):switchable_frame()
+battleinterface::battleinterface(party *travel_party, enemy_group enemy_group):switchable_frame(), battle_party_(travel_party)
 {
     travel_party_ = travel_party;
-    battle_party_ = party(travel_party_);
     enemy_group_ = enemy_group;
     create_children();
     place_children();
     connect_signals();
     queue_index_ = 0;
-    set_stylesheet("QFrame{border-color:black; border-width:2px; border-style:inset}");
     action_timer_->start(100);
 }
 
@@ -17,48 +15,123 @@ void battleinterface::create_children()
 {
     attack_ = new QPushButton("Attack");
     attack_->setEnabled(false);
-    defense_ = new QPushButton("Defense");
-    defense_->setEnabled(false);
+    defence_ = new QPushButton("Defense");
+    defence_->setEnabled(false);
     skill_ = new QPushButton("Skill");
     skill_->setEnabled(false);
+    item_ = new QPushButton("Item");
+    item_->setEnabled(false);
     return_ = new QPushButton("Return");
     return_-> setEnabled(false);
     confirm_ = new QPushButton("Confirm");
     confirm_->setEnabled(false);
-    party_view_ = new party_view(&battle_party_);
-    enemy_group_view_ = new enemy_group_view(&enemy_group_);
+    party_view_ = new party_view(this, &battle_party_);
+    enemy_group_view_ = new enemy_group_view(this, &enemy_group_);
     party_statuses_ = new group_status_panel(&battle_party_);
     enemy_group_statuses_ = new group_status_panel(&enemy_group_);
-    button_mapper_ = new QSignalMapper(this);
     action_timer_ = new QTimer(this);
 }
 void battleinterface::place_children()
 {
-    add_widget(party_view_, 9, 0, 9, 12);
-    add_widget(enemy_group_view_, 0, 8, 9, 12);
-    add_widget(party_statuses_, 13, 12, 5, 8);
-    add_widget(enemy_group_statuses_, 0, 0, 5, 8);
-    add_widget(attack_, 18, 0);
-    add_widget(defense_, 18, 1);
-    add_widget(skill_, 18, 2);
-    add_widget(return_, 18, 3);
-    add_widget(confirm_, 18, 4);
+    layout_->addWidget(party_view_, 9, 0, 9, 12);
+    layout_->addWidget(enemy_group_view_, 0, 8, 9, 12);
+    layout_->addWidget(party_statuses_, 13, 12, 5, 8);
+    layout_->addWidget(enemy_group_statuses_, 0, 0, 5, 8);
+    layout_->addWidget(attack_, 18, 0);
+    layout_->addWidget(defence_, 18, 1);
+    layout_->addWidget(skill_, 18, 2);
+    layout_->addWidget(item_, 18, 3);
+    layout_->addWidget(return_, 18, 4);
+    layout_->addWidget(confirm_, 18, 5);
 }
 void battleinterface::connect_signals()
 {
-    connect(attack_, SIGNAL(clicked()), button_mapper_, SLOT(map()));
-    connect(defense_, SIGNAL(clicked()), button_mapper_, SLOT(map()));
+    connect(attack_, SIGNAL(clicked()), this, SLOT(initiate_action()));
+    connect(defence_, SIGNAL(clicked()), this, SLOT(initiate_action()));
 
+    connect(item_, SIGNAL(clicked()), this, SLOT(select_item()));
     connect(return_, SIGNAL(clicked()), this, SLOT(repeat_move()));
     connect(confirm_, SIGNAL(clicked()), this, SLOT(take_control_from_the_player()));
-    button_mapper_->setMapping(attack_, 0);
-    button_mapper_->setMapping(defense_, 1);
-    connect(button_mapper_, SIGNAL(mapped(int)), this, SLOT(choose_target(int)));
     party_view_->prepare_status_panels_highlight(party_statuses_);
     enemy_group_view_->prepare_status_panels_highlight(enemy_group_statuses_);
-    party_view_->prepare_action_target_acquisition();
-    enemy_group_view_->prepare_action_target_acquisition();
+    /*party_view_->prepare_action_target_acquisition();
+    enemy_group_view_->prepare_action_target_acquisition();*/
     connect(action_timer_, SIGNAL(timeout()), this, SLOT(increase_actions()));
+}
+
+void battleinterface::execute_move()
+{
+    action_ = new action;
+    if(queue_index_ < action_queue_.size())
+    {
+        if(action_queue_[queue_index_].pc)
+        {
+            party_view_->get_character_frame(action_queue_[queue_index_].row, action_queue_[queue_index_].position)->update(2);
+            give_control_to_the_player(battle_party_.get_pc_address(action_queue_[queue_index_].row, action_queue_[queue_index_].position));
+        }
+        else
+        {
+            npc_act(enemy_group_.get_npc_adress(action_queue_[queue_index_].row, action_queue_[queue_index_].position));
+        }
+    }
+    else
+    {
+        restart_battle();
+    }
+}
+
+void battleinterface::give_control_to_the_player(pc *pc)
+{
+    action_->set_acting(pc);
+    attack_->setEnabled(true);
+    defence_->setEnabled(true);
+    skill_->setEnabled(true);
+    return_->setEnabled(false);
+    confirm_->setEnabled(false);
+}
+
+void battleinterface::npc_act(npc *npc)
+{
+    action_->set_acting(npc);
+    action_->set_skill(normal_attack_);
+    for(int r=0; r<2; r++)
+    {
+        for(int p=0; r<2; r++)
+        {
+            pc *potential_target = battle_party_.get_pc_address(r, p);
+            if(potential_target->alive())
+            {
+                //action_->set_targets([potential_target,nullptr,nullptr,nullptr);
+                finalize_action();
+                return;
+            }
+        }
+    }
+}
+
+void battleinterface::finalize_action()
+{
+    action_->execute();
+    delete action_;
+    party_view_->set_graphics();
+    enemy_group_view_->set_graphics();
+    party_statuses_->paste_data(&battle_party_);
+    enemy_group_statuses_->paste_data(&enemy_group_);
+    if(++queue_index_ < action_queue_.size())
+    {
+        execute_move();
+    }
+    else
+    {
+        restart_battle();
+    }
+}
+
+void battleinterface::restart_battle()
+{
+    action_queue_.clear();
+    queue_index_ = 0;
+    action_timer_->start(100);
 }
 
 void battleinterface::increase_actions()
@@ -113,137 +186,124 @@ void battleinterface::increase_actions()
     }
     if(action_queue_.size() > 0)
     {
+
         action_timer_->stop();
         execute_move();
     }
 }
 
-void battleinterface::choose_target(int group)
+void battleinterface::choose_target(skill *skill)
 {
-    action_->set_type(group);
+    if(skill->is_attack())
+    {
+        if(skill->aoe())
+        {
+            entity *targets[4] = {enemy_group_.get_npc_adress(0, 0), enemy_group_.get_npc_adress(0, 1), enemy_group_.get_npc_adress(1, 0), enemy_group_.get_npc_adress(1, 1)};
+            action_->set_targets(targets);
+            for(short r=0; r<2; r++)
+            {
+                for(short p=0; p<2; p++)
+                {
+                       enemy_group_view_->get_character_frame(r, p)->update(4);
+                }
+            }
+        }
+        else
+        {
+            for(short r=0; r<2; r++)
+            {
+                for(short p=0; p<2; p++)
+                {
+                       enemy_group_view_->get_character_frame(r, p)->update(8);
+                }
+            }
+        }
+    }
+    else
+    {
+        if(skill->aoe())
+        {
+            entity *targets[4] = {battle_party_.get_pc_address(0, 0), battle_party_.get_pc_address(0, 1), battle_party_.get_pc_address(1, 0), battle_party_.get_pc_address(1, 1)};
+            action_->set_targets(targets);
+            for(short r=0; r<2; r++)
+            {
+                for(short p=0; p<2; p++)
+                {
+                       party_view_->get_character_frame(r, p)->update(5);
+                }
+            }
+            party_view_->get_character_frame(action_queue_[queue_index_].row, action_queue_[queue_index_].position)->update(3);
+        }
+        else
+        {
+            if(skill->revive())
+            {
+                for(short r=0; r<2; r++)
+                {
+                    for(short p=0; p<2; p++)
+                    {
+                        character_frame *frame = party_view_->get_character_frame(r ,p);
+                        if(!frame->get_represented_object()->alive()){frame->update(9);}
+                    }
+                }
+            }
+            else
+            {
+                for(short r=0; r<2; r++)
+                {
+                    for(short p=0; p<2; p++)
+                    {
+                        character_frame *frame = party_view_->get_character_frame(r ,p);
+                        if(frame->get_represented_object()->alive()){frame->update(9);}
+                    }
+                }
+                character_frame *frame = party_view_->get_character_frame(action_queue_[queue_index_].row, action_queue_[queue_index_].position);
+                if(skill->is_self_targettable()){frame->update(7);}
+                else{frame->update(2);}
+            }
+        }
+    }
+    action_->set_skill(skill);
     attack_->setEnabled(false);
-    defense_->setEnabled(false);
+    defence_->setEnabled(false);
     skill_->setEnabled(false);
     return_->setEnabled(true);
-    if(group == 1)
-    {
-        for(int r=0; r<2; r++)
-        {
-            for(int p=0; p<2; p++)
-            {
-                if(battle_party_.get_pc_address(r, p)->alive())
-                {
-                    character_frame *current_frame = party_view_->get_character_frame(r, p);
-                    current_frame->set_targeting(true);
-                    current_frame->setStyleSheet("border-color:green");
-                }
-            }
-        }
-    }
-    else if(group == 0)
-    {
-        for(int r=0; r<2; r++)
-        {
-            for(int p=0; p<2; p++)
-            {
-                if(enemy_group_.get_npc_adress(r, p)->alive())
-                {
-                    character_frame *current_frame = enemy_group_view_->get_character_frame(r, p);
-                    current_frame->set_targeting(true);
-                    current_frame->setStyleSheet("border-color:yellow");
-                }
-            }
-        }
-    }
 }
 
-void battleinterface::update_target(character *new_target)
+void battleinterface::update_target(entity *new_target)
 {
     action_->set_target(new_target);
     confirm_->setEnabled(true);
 }
 
-void battleinterface::execute_move()
-{
-    action_ = new action;
-    if(queue_index_ < action_queue_.size())
-    {
-        if(action_queue_[queue_index_].pc)
-        {
-            party_view_->get_character_frame(action_queue_[queue_index_].row, action_queue_[queue_index_].position)->setStyleSheet("border-color:blue");
-            give_control_to_the_player(battle_party_.get_pc_address(action_queue_[queue_index_].row, action_queue_[queue_index_].position));
-        }
-        else
-        {
-            npc_act(enemy_group_.get_npc_adress(action_queue_[queue_index_].row, action_queue_[queue_index_].position));
-        }
-    }
-    else
-    {
-        restart_battle();
-    }
-}
 
-void battleinterface::give_control_to_the_player(pc *pc)
-{
-    action_->set_acting(pc);
-    attack_->setEnabled(true);
-    defense_->setEnabled(true);
-    skill_->setEnabled(true);
-    return_->setEnabled(false);
-    confirm_->setEnabled(false);
-}
 
 void battleinterface::take_control_from_the_player()
 {
     return_->setEnabled(false);
     confirm_->setEnabled(false);
+    party_view_->reset_states();
+    enemy_group_view_->reset_states();
     finalize_action();
 }
 
-void battleinterface::npc_act(npc *npc)
-{
-    action_->set_acting(npc);
-    action_->set_type(0);
-    for(int r=0; r<2; r++)
-    {
-        for(int p=0; r<2; r++)
-        {
-            pc *potential_target = battle_party_.get_pc_address(r, p);
-            if(potential_target->alive())
-            {
-                action_->set_target(potential_target);
-                finalize_action();
-                return;
-            }
-        }
-    }
-}
-void battleinterface::restart_battle()
-{
-    action_queue_.clear();
-    queue_index_ = 0;
-    action_timer_->start(100);
-}
 void battleinterface::repeat_move()
 {
-    delete action_;
-    execute_move();
+    party_view_->reset_states();
+    enemy_group_view_->reset_states();
+    party_view_->get_character_frame(action_queue_[queue_index_].row, action_queue_[queue_index_].position)->update(2);
+    action_->clear();
+    give_control_to_the_player(battle_party_.get_pc_address(action_queue_[queue_index_].row, action_queue_[queue_index_].position));
 }
-void battleinterface::finalize_action()
+
+void battleinterface::select_item()
 {
-    action_->execute();
-    delete action_;
-    party_view_->set_graphics(&battle_party_);
-    enemy_group_view_->set_graphics(&enemy_group_);
-    party_statuses_->paste_data(&battle_party_);
-    enemy_group_statuses_->paste_data(&enemy_group_);
-    if(++queue_index_ < action_queue_.size())
-    {
-        execute_move();
-    }
-    else
-    {
-        restart_battle();
-    }
+
+}
+
+void battleinterface::initiate_action()
+{
+    QObject *sender = QObject::sender();
+    if(sender == attack_){choose_target(normal_attack_);}
+    else if(sender == defence_){choose_target(normal_defence_);}
 }
