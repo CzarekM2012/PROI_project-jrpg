8a35a6f15ee744da7f2c118aca8da480a4bf74ef
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-05-28 16:50:43 +0200

    Upload New File

---
+++ item_storage.h
@@ -0,0 +1,19 @@
+#ifndef ITEM_STORAGE_H
+#define ITEM_STORAGE_H
+#include "equipment.h"
+#include "consumable.h"
+#include "QPair"
+class item_storage
+{
+public:
+    item_storage(), item_storage(item_storage *storage);
+    bool add_item(consumable*), add_item(equipment*);
+    equipment *get_equipment(int);
+    consumable *get_consumable(int);
+    void set_item(equipment*, unsigned int), set_item(consumable, unsigned int);
+private:
+    consumable consumables_[20]={};
+    equipment *equipment_[80]={};
+};
+
+#endif // ITEM_STORAGE_H
