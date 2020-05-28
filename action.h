ed7adc4848ea1cffb351abf125a789e862715906
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-05-28 16:43:08 +0200

    Replace action.h

--- action.h
+++ action.h
@@ -8,12 +8,13 @@ public:
     action();
     void clear();
     void execute();
-    void set_target(character *target);
-    void set_acting(character *acting);
-    void set_type(int value);
+    void set_target(entity *target);
+    void set_targets(entity *targets_array[4]);
+    void set_acting(entity *acting);
+    void set_skill(skill *skill);
 private:
-    character *target_, *acting_;
-    int type_; //attack, defense, etc.
+    entity *targets_[4]={}, *acting_;
+    skill *skill_; //attack, defense, etc.
 };
 
 #endif // ACTION_H
