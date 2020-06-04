7a3bb5147807590a5f5a4d46b7da1be342bc0066
Aleksandra Anna Piekarzewicz <a.piekarzewicz@stud.elka.pw.edu.pl>
2020-06-04 09:12:41 +0200

    Replace mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
+#include "mapinterface.h"
 
 MainWindow::MainWindow():QMainWindow()
 {
     setGeometry(QGuiApplication::primaryScreen()->geometry());
-    /*MainMenu *menu = new MainMenu(this);
-    switch_frames(menu);*/
-    battle();
+    MainMenu *menu = new MainMenu(this);
+    switch_frames(menu);
+    //battle();
+    map();
 }
 
 void MainWindow::switch_frames(switchable_frame *new_frame)
@@ -36,4 +38,15 @@ void MainWindow::battle()
     enemy_group *test_enemy_group = new enemy_group(npc_array);
     battleinterface *battle = new battleinterface(test_party, *test_enemy_group);
     switch_frames(battle);
+}
+
+void MainWindow::map()
+{
+  mapinterface *map = new mapinterface(0);
+  switch_frames(map);
+}
+
+void MainWindow::dialog()
+{
+
 }
