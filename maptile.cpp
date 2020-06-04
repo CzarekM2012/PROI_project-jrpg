5a887e4c5edf248c0575c732f33d17fdd6e6275e
Aleksandra Anna Piekarzewicz <a.piekarzewicz@stud.elka.pw.edu.pl>
2020-06-04 14:58:41 +0200

    Replace maptile.cpp

--- maptile.cpp
+++ maptile.cpp
@@ -9,14 +9,9 @@ maptile::maptile(int x, int y, QString pix, bool walk, bool event)
   coord_y = y;
   tile_w = 90; //80
   tile_h = 90; //80
-  //this->setGeometry(coord_x, coord_y, tile_w, tile_h);
   has_event = event;
   walkable = walk;
-  tile_pic_pix = QPixmap(pix);
-  tile_pic_pix = tile_pic_pix.scaled(tile_w, tile_h);
-  tile_pic = new QLabel(this);
-  tile_pic->setPixmap(tile_pic_pix);
-  //tile_pic->setFrameStyle(0);
+  setStyleSheet(QString("border-image:url(") + pix + QString(")"));
 
   up_neigh = nullptr;
   down_neigh = nullptr;
