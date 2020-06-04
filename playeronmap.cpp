4a16eaf9893c2d5818ad5a2cf8f5ccb2fd9152f8
Aleksandra Anna Piekarzewicz <a.piekarzewicz@stud.elka.pw.edu.pl>
2020-06-04 15:00:32 +0200

    Replace playeronmap.cpp

--- playeronmap.cpp
+++ playeronmap.cpp
@@ -2,15 +2,17 @@
 #include <QLabel>
 #include "maptile.h"
 
-playeronmap::playeronmap(maptile *start, QWidget *parent) : QWidget(parent)
+playeronmap::playeronmap(maptile *start, QWidget *parent)
 {
   curr_tile = start;
-  this->setGeometry(0, 0, parent->width(), parent->height());
-  face = new QLabel(this);
+  //setGeometry(0, 0, parent->width(), parent->height());
+  //face = new QLabel(this);
   //face->setGeometry(curr_tile->coord_x, curr_tile->coord_y, parent->width(), parent->height());
-
+  //face->setStyleSheet(QString("border-image:url(:/images/face1.png)"));
+/*
   QString fstring = ":/images/face1.png";
   QPixmap fimg = QPixmap(fstring);
   fimg = fimg.scaled(80, 70);
   face->setPixmap(fimg);
+  */
 }
