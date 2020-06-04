016053067e6b9f6559234a150e0fc828e3a77760
Aleksandra Anna Piekarzewicz <a.piekarzewicz@stud.elka.pw.edu.pl>
2020-06-04 15:02:30 +0200

    Replace mapinterface.h

--- mapinterface.h
+++ mapinterface.h
@@ -4,22 +4,16 @@
 #include "switchable_frame.h"
 #include "maptile.h"
 #include "playeronmap.h"
+#include "tiles_storage.h"
 
-struct tile_descriptor
-{
-  tile_descriptor(QString a, bool b = true, bool c = false)
-  {pic = a; walkable = b; has_event = c;}
-    QString pic;
-    bool walkable;
-    bool has_event;
-};
 
 class mapinterface : public switchable_frame
 {
     Q_OBJECT
 public:
-  mapinterface(int tileset);
-  std::vector<maptile*> tiles;
+  mapinterface(int tileset, int player_state);
+  tiles_storage *tiles;
+  //  std::vector<maptile*> tiles;
   QPushButton *up;
   QPushButton *down;
   QPushButton *left;
@@ -33,13 +27,15 @@ private slots:
  void slotButtonRight();
 
 private:
-  void init_tiles(int tileset);
   void set_all_neighbors();
   void set_all_events(int tileset);
   int* load_tileset(int *map, std::string path);
   void create_children();
   void place_children();
   void connect_signals();
+
+signals:
+  void change_map(int m, int p);
 
 };
 
