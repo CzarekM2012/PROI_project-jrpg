96b16ce74a78fa24090f2f9c981754713999cc68
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-06-06 18:07:54 +0200

    Replace items_display.h

--- items_display.h
+++ items_display.h
@@ -2,12 +2,26 @@
 #define ITEMS_DISPLAY_H
 #include "switchable_frame.h"
 #include "item_frame.h"
+#include "item_storage.h"
 
 class items_display : public switchable_frame
 {
+    Q_OBJECT
 public:
-    items_display();
-
+    items_display(item_storage *);
+    item_frame *get_frame(int);
+    void change_range(int value);
+    void update();
+private slots:
+    void change_displayed_type();
+    void pass_slot(entity*, int);
+signals:
+    void passed_slot(entity*, int);
+private:
+    item_frame *frames_[20];
+    item_storage *storage_;
+    int current_range_;
+    bool equipment_;
 };
 
 #endif // ITEMS_DISPLAY_H
