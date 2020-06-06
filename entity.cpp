d9e471ad40105d8c9ad29d542075d272a9e85226
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-06-06 17:58:45 +0200

    Replace entity.cpp

--- entity.cpp
+++ entity.cpp
@@ -7,9 +7,10 @@ entity::entity(entity* entity)
 {
     name_ = entity->name_;
     description_ = entity->description_;
+    memcpy(stats_, entity->stats_, 9*sizeof (int));
 }
 
-entity::entity(const char *name, const char *description): name_(name), description_(description){}
+entity::entity(const char *name, int *stats, const char *description): name_(name), description_(description){memcpy(stats_, stats, 9*sizeof (int));}
 
 entity::~entity(){}
 
@@ -17,56 +18,30 @@ QString entity::get_name(){return name_;}
 
 QString entity::get_description(){return description_;}
 
+void entity::set_name(const char *name){name_ = QString(name);}
 
-void entity::set_name(const char *name)
-{
-    name_ = QString(name);
-}
+void entity::set_name(QString name){name_ = name;}
 
-void entity::set_name(QString name)
-{
-    name_ = name;
-}
+void entity::set_description(const char *description){description_ = QString(description);}
 
-void entity::set_description(const char *description)
-{
-    description_ = QString(description);
-}
-void entity::set_description(QString description)
-{
-    description_ = description;
-}
+void entity::set_description(QString description){description_ = description;}
 
-void entity::set_max_hp(unsigned int){}
-void entity::set_current_hp(unsigned int){}
-void entity::set_max_sp(unsigned int){}
-void entity::set_current_sp(unsigned int){}
-void entity::set_atk(unsigned int){}
-void entity::set_def(unsigned int){}
-void entity::set_s_atk(unsigned int){}
-void entity::set_s_def(unsigned int){}
-void entity::set_initiative(unsigned int){}
-void entity::set_stats(unsigned int *){}
-void entity::set_stat(unsigned int, unsigned int){}
-void entity::set_stats(){}
+void entity::set_stat(int value, unsigned int index){if(index<9){stats_[index] = value;}}
+
+void entity::set_stats(int stats_array[9]){memcpy(stats_, stats_array, 9*sizeof (int));}
+
+int entity::get_stat(unsigned int index){return index<9 ? stats_[index] : -1;}
+
+void entity::get_stats(int target_array[9]){memcpy(target_array, stats_, 9*sizeof (int));}
+
 void entity::set_alive(bool){}
 void entity::set_position(int, int){}
-unsigned int entity::get_max_hp(){return 0;}
-unsigned int entity::get_current_hp(){return 0;}
-unsigned int entity::get_max_sp(){return  0;}
-unsigned int entity::get_current_sp(){return 0;}
-unsigned int entity::get_atk(){return 0;}
-unsigned int entity::get_def(){return  0;}
-unsigned int entity::get_s_atk(){return 0;}
-unsigned int entity::get_s_def(){return 0;}
-unsigned int entity::get_initiative(){return 0;}
-unsigned int entity::get_stat(unsigned int){return 0;}
-void entity::get_stats(unsigned int *){}
 bool entity::alive(){return false;}
-int entity::get_row(){return 0;}
-int entity::get_place(){return 0;}
 std::pair<short int,short int> entity::get_position(){return std::pair<short, short>();}
 void entity::trigger_statuses(){}
 void entity::expire_statuses(){}
 void entity::add_status_effect(status_effect, bool){}
 short entity::get_id(){return 0;}
+entity *entity::get_part_of_equipment(unsigned int){return nullptr;}
+short entity::get_type_of_eq(){return 0;}
+int entity::get_counter(){return 0;}
