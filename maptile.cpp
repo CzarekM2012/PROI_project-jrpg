d26c817a401ca3f04cc4cfdee5894e8c70b2d430
Aleksandra Anna Piekarzewicz <a.piekarzewicz@stud.elka.pw.edu.pl>
2020-06-06 13:58:32 +0200

    Replace maptile.cpp

--- maptile.cpp
+++ maptile.cpp
@@ -3,13 +3,21 @@
 #include <QString>
 #include <QPixmap>
 
-maptile::maptile(int x, int y, QString pix, bool walk, bool event, int nmap)
+maptile::maptile(int x, int y, QString pix, bool walk, bool event, int nmap, int d)
 {
   coord_x = x;
   coord_y = y;
   tile_w = 90; //80
   tile_h = 90; //80
-  has_event = event;
+  d_event = d;
+  if(d_event >= 0)
+    {
+      has_event = true;
+    }
+  else
+    {
+      has_event = event;
+    }
   walkable = walk;
   new_map = nmap;
   setStyleSheet(QString("border-image:url(") + pix + QString(")"));
