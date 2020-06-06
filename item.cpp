3e07ec8b4ddc6c4637e4d979094eb638d88a1ee3
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-06-06 18:05:38 +0200

    Replace item.cpp

--- item.cpp
+++ item.cpp
@@ -4,6 +4,6 @@ item::item():entity(), id_(-1){}
 
 item::item(item* item):entity(item), id_(item->id_){}
 
-item::item(const char* name, const char *description, const short id):entity(name, description), id_(id){}
+item::item(const char* name, int*stats, const short id, const char *description):entity(name, stats, description), id_(id){}
 
 short item::get_id(){return id_;}
