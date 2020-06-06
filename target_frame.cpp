aeb97a1c70c6ffcc648ffe1a8b34ea019bc8d8b7
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-06-06 18:14:43 +0200

    Replace target_frame.cpp

--- target_frame.cpp
+++ target_frame.cpp
@@ -3,30 +3,22 @@
 
 target_frame::target_frame():QFrame(){setMouseTracking(true);}
 
+target_frame::target_frame(entity *entity):target_frame(){represented_object_ = entity;}
+
 target_frame::~target_frame(){}
 
-void target_frame::set_represented_object(character *character){represented_object_ = character;}
-
-void target_frame::set_represented_object(item *item){represented_object_ = item;}
+void target_frame::set_represented_object(entity *entity){represented_object_ = entity;}
 
 entity* target_frame::get_represented_object(){return represented_object_;}
-
-/*void target_frame::set_compound_style_sheet(QVector<QString> array_of_traits)
-{
-    QString stylesheet = array_of_traits[0];
-    for(int i=1; i<array_of_traits.size(); i++)
-    {
-        stylesheet+= QString("; ") + array_of_traits[i];
-    }
-    setStyleSheet(stylesheet);
-}*/
 
 void target_frame::set_standard_graphic(const char *type_of_graphic)
 {
     if(represented_object_ != nullptr)
     {
         setStyleSheet(QString("border-image:url(:/images/") + represented_object_->get_name() + QString(type_of_graphic) + QString(".png)"));
+        return;
     }
+    setStyleSheet("");
 }
 
 void target_frame::enterEvent(QEvent *event)
@@ -34,6 +26,7 @@ void target_frame::enterEvent(QEvent *event)
     emit hover_in();
     QWidget::enterEvent(event);
 }
+
 void target_frame::leaveEvent(QEvent *event)
 {
     emit hover_out();
