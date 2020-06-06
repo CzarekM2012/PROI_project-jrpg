1ce60636ce72f032606ae1512aaf6cbc92375f61
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-06-06 18:12:56 +0200

    Replace pc_choice_panel.h

--- pc_choice_panel.h
+++ pc_choice_panel.h
@@ -1,15 +1,16 @@
 #ifndef PC_CHOICE_PANEL_H
 #define PC_CHOICE_PANEL_H
 #include "switchable_frame.h"
-#include "party.h"
-#include "target_frame.h"
+#include "party_display.h"
+#include <QPushButton>
 
 class pc_choice_panel : public switchable_frame
 {
 public:
     pc_choice_panel(party *);
 private:
-    target_frame *formation_[3][2]={};
+    party_display *display_;
+    QPushButton *cancel_, confirm_;
 };
 
 #endif // PC_CHOICE_PANEL_H
