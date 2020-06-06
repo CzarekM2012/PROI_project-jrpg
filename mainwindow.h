a3cc6f1e8ebd4d9b9408a94be03019aaf1040add
Aleksandra Anna Piekarzewicz <a.piekarzewicz@stud.elka.pw.edu.pl>
2020-06-06 13:56:27 +0200

    Replace mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -10,13 +10,18 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 public:
     MainWindow();
+    QWidget *hidden_frame;
+    switchable_frame *curr_map;
 
 public slots:
     void battle();
     void map(int tileset, int player_pos);
-    void dialog();
+    void dialog(int dnum);
+    void end_dialog();
 private:
     void switch_frames(switchable_frame *new_frame);
+    void hide_frame(switchable_frame *new_frame);
+    void restore_frame();
 };
 
 #endif // MAINWINDOW_H
