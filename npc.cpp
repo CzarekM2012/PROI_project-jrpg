981084d95d37101db6f44715cc22bec8f373c837
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-06-06 18:10:05 +0200

    Replace npc.cpp

--- npc.cpp
+++ npc.cpp
@@ -2,5 +2,5 @@
 
 npc::npc():character(){}
 
-npc::npc(const char *name, const char *description, unsigned int *stats_array):character(name, description, stats_array){}
+npc::npc(const char *name, int *stats, const char *description):character(name, stats, description){}
 
