c32b3487ffce59b4b316dc3cd10eed919fa11779
Aleksandra Anna Piekarzewicz <a.piekarzewicz@stud.elka.pw.edu.pl>
2020-06-06 13:56:51 +0200

    Replace playeronmap.cpp

--- playeronmap.cpp
+++ playeronmap.cpp
@@ -5,14 +5,5 @@
 playeronmap::playeronmap(maptile *start, QWidget *parent)
 {
   curr_tile = start;
-  //setGeometry(0, 0, parent->width(), parent->height());
-  //face = new QLabel(this);
-  //face->setGeometry(curr_tile->coord_x, curr_tile->coord_y, parent->width(), parent->height());
-  //face->setStyleSheet(QString("border-image:url(:/images/face1.png)"));
-/*
-  QString fstring = ":/images/face1.png";
-  QPixmap fimg = QPixmap(fstring);
-  fimg = fimg.scaled(80, 70);
-  face->setPixmap(fimg);
-  */
+
 }
