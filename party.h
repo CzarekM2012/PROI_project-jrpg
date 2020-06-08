90fa060545a9af3970457a204edcb956a6f34b58
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-06-09 00:08:10 +0200

    Upload New File

---
+++ party.h
@@ -0,0 +1,19 @@
+#ifndef PARTY_H
+#define PARTY_H
+#include "pc.h"
+#include "item_storage.h"
+
+class party
+{
+public:
+    party(), party(pc *pcs), party(party *party_to_copy);
+    void add_member(pc *added_pc);
+    pc *get_pc_address(int r, int p);
+    void swap_members(std::pair<int,int>, std::pair<int,int>);
+    item_storage *get_item_storage();
+private:
+    pc formation_[3][2]={}; //frontline - backline - reserve
+    item_storage *item_storage_;
+};
+
+#endif // PARTY_H
