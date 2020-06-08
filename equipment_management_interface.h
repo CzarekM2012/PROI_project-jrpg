0b430db0921aea01805e1ce52a79a3b449013fc9
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-06-09 00:04:46 +0200

    Upload New File

---
+++ equipment_management_interface.h
@@ -0,0 +1,46 @@
+#ifndef ECQUIPMENT_MANAGEMENT_INTERFACE_H
+#define ECQUIPMENT_MANAGEMENT_INTERFACE_H
+#include "switchable_frame.h"
+#include "party.h"
+#include "party_display.h"
+#include "full_stats_display.h"
+#include "items_display.h"
+#include "pc_equipment_display.h"
+#include "skill_choice_panel.h"
+#include <QPushButton>
+
+class equipment_management_interface : public switchable_frame
+{
+    Q_OBJECT
+public:
+    equipment_management_interface(), equipment_management_interface(party *party);
+private slots:
+    void pass_character(entity*);
+    void enable_equip();
+    void disable_equip();
+    void item_choice(entity*, int);
+    void change_item_types();
+    void use_item();
+    void prepare_positions_switch();
+    void switch_positions(entity*);
+    //void close();
+signals:
+    void passed_character(pc*);
+    void passed_equipment(entity*);
+private:
+    void create_widgets();
+    void place_widgets();
+    void connect_signals();
+    party *party_;
+    party_display *party_display_;
+    skill_choice_panel *skills_display;
+    full_stats_display *stats_display_;
+    items_display *items_display_;
+    pc_equipment_display *pc_display_;
+    QPushButton *switch_places_, *equip_or_use_, *turn_equipment_page_right_, *turn_equipment_page_left_, *switch_item_types_, *close_;
+    pc *choosen_character_;
+    entity *choosen_item_;
+    int choosen_storage_slot_;
+};
+
+#endif // ECQUIPMENT_MANAGEMENT_INTERFACE_H
