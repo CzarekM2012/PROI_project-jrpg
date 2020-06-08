3fcf47c0adbc1c552b4a210b354b53fdd1e0a43f
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-06-09 00:01:41 +0200

    Upload New File

---
+++ action.h
@@ -0,0 +1,21 @@
+#ifndef ACTION_H
+#define ACTION_H
+#include "character.h"
+
+class action
+{
+public:
+    action();
+    void clear();
+    bool execute();
+    entity *get_acting();
+    void set_target(entity *target);
+    void set_targets(entity *targets_array[4]);
+    void set_acting(entity *acting);
+    void set_skill(skill *skill);
+private:
+    entity *targets_[4]={}, *acting_;
+    skill *skill_; //attack, defense, etc.
+};
+
+#endif // ACTION_H
