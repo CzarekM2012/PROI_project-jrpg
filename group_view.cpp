4fb69a4316507f9ee337897d4346fd6ee96127d5
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-06-09 00:05:19 +0200

    Upload New File

---
+++ group_view.cpp
@@ -0,0 +1,65 @@
+#include "group_view.h"
+
+group_view::group_view():switchable_frame()
+{
+    for(int r=0; r<2; r++)
+    {
+        for(int p=0; p<2; p++)
+        {
+            formation_[r][p] = new character_frame();
+            connect(this, SIGNAL(target_info(entity *)), formation_[r][p], SLOT(is_target(entity *)));
+            connect(formation_[r][p], SIGNAL(target_acquired(entity *)), this, SLOT(pass_target_info(entity *)));
+        }
+    }
+}
+
+character_frame *group_view::get_character_frame(short int r, short int p){return formation_[r][p];}
+
+void group_view::set_graphics()
+{
+    for(int r=0; r<2; r++)
+      {
+        for(int p=0;p<2;p++)
+          {
+            if(formation_[r][p]->get_represented_object()->valid())
+              {
+                if(formation_[r][p]->get_represented_object()->alive())
+                  {formation_[r][p]->set_standard_graphic();}
+                else{formation_[r][p]->set_standard_graphic("-dead");}
+              }
+          }
+
+      }
+ }
+
+void group_view::prepare_status_panels_highlight(group_status_panel *group_status_panel)
+{
+    for(int r=0; r<2; r++)
+    {
+        for(int p=0; p<2; p++)
+        {
+            if(formation_[r][p]->state()!=0)
+            {
+                character_status_panel *character_status_panel = group_status_panel->get_character_status_panel(r, p);
+                connect(formation_[r][p], SIGNAL(hover_in()), character_status_panel, SLOT(highlight_on()));
+                connect(formation_[r][p], SIGNAL(hover_out()), character_status_panel, SLOT(highlight_off()));
+            }
+        }
+    }
+}
+
+void group_view::reset_states()
+{
+    for(int r=0; r<2; r++)
+    {
+        for(int p=0; p<2; p++)
+        {
+            formation_[r][p]->update(1);
+        }
+    }
+}
+
+void group_view::pass_target_info(entity *entity)
+{
+    target_info(entity);
+}
