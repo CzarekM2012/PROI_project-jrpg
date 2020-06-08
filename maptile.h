2216b3d525028babc2af0b23e7cd8dc599842aaf
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-06-09 00:07:43 +0200

    Upload New File

---
+++ maptile.h
@@ -0,0 +1,29 @@
+#ifndef MAPTILE_H
+#define MAPTILE_H
+#include <QFrame>
+#include <QLabel>
+
+
+class maptile: public QFrame
+{
+  Q_OBJECT
+public:
+  maptile(int x, int y, QString pix, bool walk = true, bool event = false, int nmap = -1, int d = -1);
+  int coord_x, coord_y;
+  int tile_w, tile_h, new_map, d_event;
+  bool has_event, walkable, is_curr_tile = false;
+  QPixmap tile_pic_pix;
+  QLabel *tile_pic;
+
+  maptile *up_neigh;
+  maptile *down_neigh;
+  maptile *l_neigh;
+  maptile *r_neigh;
+
+  void set_neighbors(maptile *up, maptile *down, maptile *l, maptile *r);
+
+signals:
+
+};
+
+#endif // MAPTILE_H
