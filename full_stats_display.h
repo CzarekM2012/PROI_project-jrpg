892b378b7e5e173b487502d1ec08e462a97d41a6
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-06-09 00:04:58 +0200

    Upload New File

---
+++ full_stats_display.h
@@ -0,0 +1,28 @@
+#ifndef FULL_STATS_DISPLAY_H
+#define FULL_STATS_DISPLAY_H
+#include "pc.h"
+#include "switchable_frame.h"
+#include "data_processing.h"
+#include "label_with_icon.h"
+
+class full_stats_display : public switchable_frame
+{
+    Q_OBJECT
+public:
+    full_stats_display();
+    void display_item(entity*);
+    void update_stats();
+    void clear();
+private slots:
+    void display_character(pc*);
+
+signals:
+    void unlock_equip();
+    void lock_equip();
+private:
+    pc* represented_character_=nullptr;
+    QLabel *name_;
+    label_with_icon* stat_[7];
+};
+
+#endif // FULL_STATS_DISPLAY_H
