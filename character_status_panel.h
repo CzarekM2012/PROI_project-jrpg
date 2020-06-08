1e43b9e7d8632328bee04d76f9e45a6c130627d1
Cezary Przemysław Moczulski <c.moczulski@stud.elka.pw.edu.pl>
2020-06-09 00:02:45 +0200

    Upload New File

---
+++ character_status_panel.h
@@ -0,0 +1,31 @@
+#ifndef CHARACTER_STATUS_PANEL_H
+#define CHARACTER_STATUS_PANEL_H
+#include <QFrame>
+#include <QLabel>
+#include <QProgressBar>
+#include <QGridLayout>
+#include "pc.h"
+#include "npc.h"
+#include "data_processing.h"
+#include "label_with_icon.h"
+
+class character_status_panel: public QFrame
+{
+    Q_OBJECT
+public:
+    character_status_panel();
+    void paste_data(character *character);
+    QProgressBar *get_action_bar_();
+    void clear();
+private slots:
+    void highlight_on();
+    void highlight_off();
+
+private:
+    QLabel *name_;
+    label_with_icon *hp_, *sp_;
+    QProgressBar *action_;
+    QGridLayout *layout_;
+};
+
+#endif // CHARACTER_STATUS_PANEL_H
